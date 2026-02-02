a92209f030ecb62cc4400d4ca6a96818bebdb7d8
NikanorovVD <nikanorovvsevolod1@gmail.com>
2026-02-02 17:52:40 +0300

    Editor-only logging

--- Assets/Scenes/Scripts/LogWriter.cs
+++ Assets/Scenes/Scripts/LogWriter.cs
@@ -1,19 +1,21 @@
 using System;
 using System.IO;
 
-public class LogWriter 
+public class LogWriter
 {
     const string LogFilePath = "log.txt";
 
-    public static void WriteLog(string messeage)
+    public static void WriteLog(string message)
     {
+#if UNITY_EDITOR
         try
         {
             using (StreamWriter LogStream = new StreamWriter(LogFilePath, true))
             {
-                LogStream.WriteLine(messeage);
+                LogStream.WriteLine(message);
             }
         }
         catch (Exception) { }
+#endif
     }
 }
